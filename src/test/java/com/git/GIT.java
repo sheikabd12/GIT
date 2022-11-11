8431e6aa65bcb23dc6d186d5f95636c3658d53b8
sheikabd12 <mstsheik@gmail.com>
2022-11-11 15:54:04 +0530

    msg

---
+++ src/test/java/com/git/GIT.java
@@ -0,0 +1,15 @@
+package com.git;
+
+public class GIT {
+	
+	public void class1() {
+	    System.out.println("1");	
+	}
+	public void class2() {
+		System.out.println("2");	
+	}
+	public void class3() {
+		System.out.println("3");
+	}
+
+}
